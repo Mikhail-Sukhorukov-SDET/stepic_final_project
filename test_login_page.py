0d4bddcd1ec75dfbac59ee937d9218755fde997d
Mikhail <hrumqa@gmail.com>
2020-01-08 18:30:46 +0300

    Update test_login_page.py

--- test_login_page.py
+++ test_login_page.py
@@ -1,7 +1,9 @@
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
+import pytest
 
 
+@pytest.mark.xfail(reason="Main page contain different login link #registration_link from other pages #login_link")
 def test_guest_should_see_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     main_page = MainPage(browser, link)
