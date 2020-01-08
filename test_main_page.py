573dfbeeeaae5a03e6440edbc7c0f0970b7c3382
Mikhail <hrumqa@gmail.com>
2020-01-08 16:12:05 +0300

    created class TestLoginFromMainPage for main page tests

--- test_main_page.py
+++ test_main_page.py
@@ -4,22 +4,23 @@ from pages.basket_page import BasketPage
 import pytest
 
 
-@pytest.mark.xfail(reason="Main page contain different login link #registration_link from other pages #login_link")
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.go_to_login_page()
-    login_page = LoginPage(browser, link)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    #@pytest.mark.xfail(reason="Main page contain different login link #registration_link from other pages #login_link")
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, link)
+        login_page.should_be_login_page()
 
-
-@pytest.mark.xfail(reason="Main page contain different login link #registration_link from other pages #login_link")
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_login_link()
+    #@pytest.mark.xfail(reason="Main page contain different login link #registration_link from other pages #login_link")
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
