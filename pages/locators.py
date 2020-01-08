0d868f18bfe389df1d56a0362385dff4f90f9383
Mikhail <hrumqa@gmail.com>
2020-01-08 03:08:17 +0300

    updated architect of go to login page

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTRATION_FORM = (By.ID, "register_form")
@@ -16,4 +12,10 @@ class ProductPageLocators():
     PRICE_OF_PRODUCT = (By.CSS_SELECTOR, "div.product_main p.price_color")
     AFTER_ADDING_PRODUCT_NAME_TEXT = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
     AFTER_ADDING_BASKET_PRICE_TEXT = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
 
 
@@ -53,3 +54,23 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.adding_product_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.should_disappear_after_adding_messages()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
+
+
+
+
